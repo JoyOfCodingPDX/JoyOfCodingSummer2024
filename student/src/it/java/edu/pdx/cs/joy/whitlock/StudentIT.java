23b59eca9bdbffc124a2dabda158b3d01be68773
David Whitlock <david.m.whitlock@gmail.com>
2024-07-03 18:37:24 -0700

    Added an integration test for the "Dave" student.

--- student/src/it/java/edu/pdx/cs/joy/whitlock/StudentIT.java
+++ student/src/it/java/edu/pdx/cs/joy/whitlock/StudentIT.java
@@ -1,9 +1,11 @@
 package edu.pdx.cs.joy.whitlock;
 
 import edu.pdx.cs.joy.InvokeMainTestCase;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
 
 /**
@@ -15,8 +17,20 @@ class StudentIT extends InvokeMainTestCase {
 
   @Test
   void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
-    InvokeMainTestCase.MainMethodResult result = invokeMain(Student.class);
+    InvokeMainTestCase.MainMethodResult result = invokeMain();
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+  @Disabled
+  @Test
+  void daveStudentHasExpectedOutput() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain("Dave", "male", "3.64", "Algorithms", "Operating Systems", "Java");
+    assertThat(result.getTextWrittenToStandardError(), equalTo(""));
+    assertThat(result.getTextWrittenToStandardOut(), containsString("Dave has a GPA of 3.64 and is taking 3 classes: Algorithms, Operating Systems, and Java.  He says \"This class is too much work\"."));
+  }
+
+  private MainMethodResult invokeMain(String... args) {
+    return invokeMain(Student.class, args);
   }
 
 }
